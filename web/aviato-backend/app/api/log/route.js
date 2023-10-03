9e35c069963336f2c8feba61548c15302b666068
v4nderstruck <zensayyy@gmail.com>
2023-10-03 17:43:41 +0200

    web/aviato-backend error log endpoint

--- web/aviato-backend/app/api/log/route.js
+++ web/aviato-backend/app/api/log/route.js
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 
 export async function GET(req) {
   // TODO: Implement the log API endpoint, see readme for details
-  return NextResponse.json({});
+  return NextResponse.json([]);
 }
